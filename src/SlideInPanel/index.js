1f8899a61cae8434b71290394fdfe5e4f8c14227
Avi <avrahamhcn@gmail.com>
2021-07-04 14:13:31 +0100

    make component transition out too

--- src/SlideInPanel/index.js
+++ src/SlideInPanel/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SlideInPanel.css';
 
-const SlideInPanel = ({ from, size, isOpen }) => {
+const SlideInPanel = ({ from, size, isOpen, children, handleBackdrop }) => {
 
     const isVertical = from === 'top' || from === 'bottom';
     const isHorizontal = from === 'right' || from === 'left';
@@ -15,7 +15,7 @@ const SlideInPanel = ({ from, size, isOpen }) => {
             return `${size}%`;
         }
 
-        return '0';
+        return '0%';
     };
 
     const determineHeight = () => {
@@ -27,32 +27,34 @@ const SlideInPanel = ({ from, size, isOpen }) => {
             return `${size}%`;
         }
 
-        return '0';
+        return '0%';
     };
 
-    
-
     return (
-        <div
-            className="backdrop"
-            style={{
-                width: isOpen ? `100vw` : 0,
-            }}
-        >
+        <>
             <div
-                className="panel"
+                className="backdrop"
+                onClick={() => handleBackdrop()}
                 style={{
-                    width: determineWidth(),
-                    height: determineHeight(),
-                    top: from === 'top' ? 0 : '',
-                    right: from === 'right' ? 0 : '',
-                    bottom: from === 'bottom' ? 0 : '',
-                    left: from === 'left' ? 0 : '',
+                    pointerEvents: isOpen ? 'auto' : 'none',
+                    backgroundColor: isOpen ? 'rgba(51, 51, 51, 0.7)' : 'transparent',
                 }}
             >
-
+                <div
+                    className="panel"
+                    style={{
+                        width: determineWidth(),
+                        height: determineHeight(),
+                        top: from === 'top' ? 0 : '',
+                        right: from === 'right' ? 0 : '',
+                        bottom: from === 'bottom' ? 0 : '',
+                        left: from === 'left' ? 0 : '',
+                    }}
+                >
+                    {children}
+                </div>
             </div>
-        </div>
+        </>
     )
 };
 
@@ -60,6 +62,8 @@ SlideInPanel.defaultProps = {
     isOpen: false,
     size: 60,
     from: 'left',
+    children: null,
+    handleBackdrop: null,
 };
 
 export default SlideInPanel;
